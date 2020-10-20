85b30d5861af66e490c09bb4e7e7a6011146c809
Manal BENCHRIF <72361536+ManalBenchrif@users.noreply.github.com>
2020-10-20 01:55:29 +0100

    Add files via upload

---
+++ operationWithList.py
@@ -0,0 +1,7 @@
+#Lists can be joined with the addition operators:
+list1=[1,2,3]
+list2=[4,5]
+list_result=list1+list2
+print(list_result)
+#Like strings, Python supports forming new lists with a repeating sequence using the multiplication operator:
+print(list_result*2)
